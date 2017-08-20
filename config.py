b7c2a251e82a915386c0904f1b5e73cefbcc3d33
Piotr Szczepanski <peter@192-168-1-12.tpgi.com.au>
2017-08-20 22:55:19 +1000

    tests for login endpoint implemented

--- config.py
+++ config.py
@@ -11,3 +11,7 @@ class Config(object):
     GITHUB_API_CREATE_PULL_REQUEST = "https://api.github.com/repos/{owner}/{repos}/pulls"
     GITHUB_API_CREATE_REVIEW_REQUEST = "https://api.github.com/repos/{owner}/{repo}/pulls/{number}/requested_reviewers"
     GITHUB_USER_LOGIN_ENDPOINT = "https://api.github.com/user"
+
+
+class TestConfig(Config):
+    TESTING = True
---
+++ tests/test_users.py
@@ -0,0 +1,58 @@
+"""
+Users endpoints tests.
+"""
+
+import requests
+import json
+from flask import Flask
+from flask_testing import TestCase, LiveServerTestCase
+
+
+class TestCaseUser(TestCase):
+
+    def create_app(self):
+
+        app = Flask(__name__)
+        app.config.from_object('config.TestConfig')
+        from api.users import GetFollowers, Login
+        from api.repos import CreatePullRequest
+        from flask_restful import Api
+        api = Api(app)
+        api.add_resource(GetFollowers, '/users/<string:user>')
+        api.add_resource(Login, '/users/login/')
+        api.add_resource(CreatePullRequest, '/repos/create_pull_request/')
+
+        return app
+
+    def test_correct_login_and_password(self):
+        post_data = {'login': '',
+                     'password': ''}
+        response = self.client.post("/users/login/", data=post_data)
+        self.assertEqual(response.status_code, 200)
+
+    def test_correct_login_incorrect_password(self):
+        post_data = {'login': 'cypu',
+                     'password': 'pass1'}
+        response = self.client.post("/users/login/", data=post_data)
+        self.assertEqual(response.status_code, 401)
+
+    def test_incorrect_login_and_password(self):
+        post_data = {'login': 'this_is_incorrect_login',
+                     'password': 'this_is_incorrect_password'}
+        response = self.client.post("/users/login/", data=post_data)
+        self.assertEqual(response.status_code, 401)
+
+    def test_request_without_login(self):
+        post_data = {'password': 'this_is_incorrect_password'}
+        response = self.client.post("/users/login/", data=post_data)
+        self.assertEqual(response.status_code, 401)
+
+    def test_request_without_password(self):
+        post_data = {'login': 'cypu'}
+        response = self.client.post("/users/login/", data=post_data)
+        self.assertEqual(response.status_code, 401)
+
+    def test_request_without_login_password(self):
+        post_data = {}
+        response = self.client.post("/users/login/", data=post_data)
+        self.assertEqual(response.status_code, 401)
