e2afdacdfa25c962c13f0737ea70026044c5f4e5
Piotr Szczepanski <peter@192-168-1-12.tpgi.com.au>
2017-08-21 09:49:32 +1000

    loading test configuration from config file

--- tests/test_repos.py
+++ tests/test_repos.py
@@ -4,10 +4,12 @@ Repos endpoints tests.
 
 import requests
 import json
+import os
 from flask import Flask
 from flask_testing import TestCase
 from flask_restful import Api
 from api.repos import CreatePullRequest
+from api.users import Login
 
 
 class TestCaseRepo(TestCase):
@@ -18,8 +20,19 @@ class TestCaseRepo(TestCase):
         app.config.from_object('config.TestConfig')
         api = Api(app)
         api.add_resource(CreatePullRequest, '/repos/create_pull_request/')
-
+        api.add_resource(Login, '/users/login/')
+        self.load_test_config()
         return app
+
+    def load_test_config(self):
+
+        test_config = os.path.join(os.getcwd(), 'test_config')
+        self.credentials = {}
+
+        with open(test_config, 'r') as f:
+            for line in f.readlines():
+                k ,v = line.split(':')
+                self.credentials.update({k.strip(): v.strip()})
 
     def test_pull_request_without_auth_token(self):
         post_data = {}
@@ -30,13 +43,18 @@ class TestCaseRepo(TestCase):
 
     def test_pull_request_correct_data(self):
 
+        post_data = {'login': self.credentials.get('login'),
+                     'password': self.credentials.get('password')}
+        response = self.client.post("/users/login/", data=post_data)
+        token = json.loads(response.data)['token']
+
         post_data = {
-            'changeset': '',
-            'repository': '',
-            'title': '',
+            'changeset': 'test',
+            'repository': '{}/{}'.format(self.credentials.get('login'), self.credentials.get('repository')),
+            'title': 'example pull request',
             'base': 'master',
-            'token': '',
-            'reviewers': ''
+            'token': token,
+            'reviewers': 'notempty'
         }
         response = self.client.post("/repos/create_pull_request/", data=post_data)
         self.assertEqual(response.status_code, 201)
--- tests/test_users.py
+++ tests/test_users.py
@@ -3,53 +3,64 @@ Users endpoints tests.
 """
 
 import requests
+import os
 from flask import Flask
 from flask_testing import TestCase
 from api.users import GetFollowers, Login
 from flask_restful import Api
 
 
-# class TestCaseUser(TestCase):
-#
-#     def create_app(self):
-#
-#         app = Flask(__name__)
-#         app.config.from_object('config.TestConfig')
-#         api = Api(app)
-#         api.add_resource(GetFollowers, '/users/<string:user>')
-#         api.add_resource(Login, '/users/login/')
-#
-#         return app
-#
-#     def test_correct_login_and_password(self):
-#         post_data = {'login': '',
-#                      'password': ''}
-#         response = self.client.post("/users/login/", data=post_data)
-#         self.assertEqual(response.status_code, 200)
-#
-#     def test_correct_login_incorrect_password(self):
-#         post_data = {'login': 'cypu',
-#                      'password': 'pass1'}
-#         response = self.client.post("/users/login/", data=post_data)
-#         self.assertEqual(response.status_code, 401)
-#
-#     def test_incorrect_login_and_password(self):
-#         post_data = {'login': 'this_is_incorrect_login',
-#                      'password': 'this_is_incorrect_password'}
-#         response = self.client.post("/users/login/", data=post_data)
-#         self.assertEqual(response.status_code, 401)
-#
-#     def test_request_without_login(self):
-#         post_data = {'password': 'this_is_incorrect_password'}
-#         response = self.client.post("/users/login/", data=post_data)
-#         self.assertEqual(response.status_code, 401)
-#
-#     def test_request_without_password(self):
-#         post_data = {'login': 'cypu'}
-#         response = self.client.post("/users/login/", data=post_data)
-#         self.assertEqual(response.status_code, 401)
-#
-#     def test_request_without_login_password(self):
-#         post_data = {}
-#         response = self.client.post("/users/login/", data=post_data)
-#         self.assertEqual(response.status_code, 401)
+class TestCaseUser(TestCase):
+
+    def create_app(self):
+
+        app = Flask(__name__)
+        app.config.from_object('config.TestConfig')
+        api = Api(app)
+        api.add_resource(GetFollowers, '/users/<string:user>')
+        api.add_resource(Login, '/users/login/')
+        self.load_test_config()
+        return app
+
+    def load_test_config(self):
+
+        p = os.path.join(os.getcwd(), 'test_config')
+        self.credentials = {}
+
+        with open(p, 'r') as f:
+            for l in f.readlines():
+                k,v = l.split(':')
+                self.credentials.update({k.strip(): v.strip()})
+
+    def test_correct_login_and_password(self):
+        post_data = {'login': self.credentials.get('login'),
+                     'password': self.credentials.get('password')}
+        response = self.client.post("/users/login/", data=post_data)
+        self.assertEqual(response.status_code, 200)
+
+    def test_correct_login_incorrect_password(self):
+        post_data = {'login': 'cypu',
+                     'password': 'pass1'}
+        response = self.client.post("/users/login/", data=post_data)
+        self.assertEqual(response.status_code, 401)
+
+    def test_incorrect_login_and_password(self):
+        post_data = {'login': 'this_is_incorrect_login',
+                     'password': 'this_is_incorrect_password'}
+        response = self.client.post("/users/login/", data=post_data)
+        self.assertEqual(response.status_code, 401)
+
+    def test_request_without_login(self):
+        post_data = {'password': 'this_is_incorrect_password'}
+        response = self.client.post("/users/login/", data=post_data)
+        self.assertEqual(response.status_code, 401)
+
+    def test_request_without_password(self):
+        post_data = {'login': 'cypu'}
+        response = self.client.post("/users/login/", data=post_data)
+        self.assertEqual(response.status_code, 401)
+
+    def test_request_without_login_password(self):
+        post_data = {}
+        response = self.client.post("/users/login/", data=post_data)
+        self.assertEqual(response.status_code, 401)
