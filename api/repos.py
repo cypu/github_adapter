691629857f758a9a5f43c909415157c9fed21fd3
Piotr Szczepanski <peter@192-168-1-12.tpgi.com.au>
2017-08-20 19:12:13 +1000

    Review request endpoint added + check auth token for pull request

--- api/repos.py
+++ api/repos.py
@@ -3,7 +3,6 @@ API endpoints related to repositories.
 """
 
 import requests
-import base64
 import json
 from flask_restful import Resource, reqparse
 from . import app
@@ -23,18 +22,23 @@ class CreatePullRequest(Resource):
         parser.add_argument('changeset')
         parser.add_argument('repository')
         parser.add_argument('title')
+        parser.add_argument('token')
 
         args = parser.parse_args()
-        repo_owner, repo_name = args['repository'].split('/')  # full repository name format : 'owner/repo_name'
 
-        userandpasw = base64.urlsafe_b64encode(bytes("%s:%s" % ('', ''), 'utf-8')).decode('ascii')
+        if args.get('token'):
+            repo_owner, repo_name = args['repository'].split('/')  # full repository name format : 'owner/repo_name'
 
-        post_data = {"title": args['title'] or "This is a title of pull request",
-                     "body": args.get('body') or "This is a pull request.",
-                     "head": '{}:{}'.format(repo_owner,args['changeset']),  # Regards to Github API documentation, changeset is the branch name
-                     "base": "master"}
+            post_data = {"title": args.get('title') or "This is a title of pull request",
+                         "body": args.get('body') or "This is a pull request.",
+                         "head": '{}:{}'.format(repo_owner, args['changeset']),
+                         # Regards to Github API documentation, changeset is the branch name
+                         "base": "master"}
 
-        headers = {'Authorization': 'Basic ' + userandpasw}
-        r = requests.post(app.config['GITHUB_API_CREATE_PULL_REQUEST'].format(owner=repo_owner, repos=repo_name), data=json.dumps(post_data), headers=headers)
+            headers = {'Authorization': 'Basic ' + userandpasw}
+            r = requests.post(app.config['GITHUB_API_CREATE_PULL_REQUEST'].format(owner=repo_owner, repos=repo_name),
+                              data=json.dumps(post_data), headers=headers)
 
-        return
+            return r.json(), r.status_code
+        else:
+            return {'message': 'Missing authorization token'}, 401
--- config.py
+++ config.py
@@ -9,4 +9,5 @@ class Config(object):
     GITHUB_API_USER_DETAILS = "https://api.github.com/users/{}"
     GITHUB_API_USER_FOLLOWERS = "https://api.github.com/users/{}/followers"
     GITHUB_API_CREATE_PULL_REQUEST = "https://api.github.com/repos/{owner}/{repos}/pulls"
+    GITHUB_API_CREATE_REVIEW_REQUEST = "https://api.github.com/repos/{owner}/{repo}/pulls/{number}/requested_reviewers"
     GITHUB_USER_LOGIN_ENDPOINT = "https://api.github.com/user"
