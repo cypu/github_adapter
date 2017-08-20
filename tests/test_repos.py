489770f431ef033718929e68fe029989c6689cda
Piotr Szczepanski <peter@192-168-1-12.tpgi.com.au>
2017-08-20 23:27:11 +1000

    testing pull_request without auth token added

---
+++ tests/test_repos.py
@@ -0,0 +1,29 @@
+"""
+Repos endpoints tests.
+"""
+
+import requests
+import json
+from flask import Flask
+from flask_testing import TestCase
+from flask_restful import Api
+from api.repos import CreatePullRequest
+
+
+class TestCaseUser(TestCase):
+
+    def create_app(self):
+
+        app = Flask(__name__)
+        app.config.from_object('config.TestConfig')
+        api = Api(app)
+        api.add_resource(CreatePullRequest, '/repos/create_pull_request/')
+
+        return app
+
+    def test_pull_request_without_auth_token(self):
+        post_data = {}
+        response = self.client.post("/repos/create_pull_request/", data=post_data)
+        self.assertEqual(response.status_code, 401)
+        json.loads(response.data)
+        self.assertEqual(json.loads(response.data), {'message': 'Missing authorization token'})
